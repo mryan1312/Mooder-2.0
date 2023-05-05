7d05be904150751575d0bd4a48e30e7be21389aa
mryan1312 <mryan1312@outlook.com>
2023-05-05 06:48:21 -0500

    encryption, decryption fixed, plotting implemented

--- Crypto/Crypto.py
+++ Crypto/Crypto.py
@@ -1,4 +1,5 @@
 import tkinter
+import bytes
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -6,19 +7,17 @@ import base64
 import os
 import csv
 import datetime
-from tkinter.tix import PopupMenu
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-
+import pandas as pd
+import plotly.express as px
 
 def password_to_key(password):                                      # Takes password to generate key
-    salt = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
-        salt=salt,
+        salt=b'',
         iterations=390000,
     )
     key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
@@ -26,7 +25,7 @@ def password_to_key(password):                                      # Takes pass
 
 def encrypt(key, data):
     f = Fernet(key)
-    token = f.encrypt(data.encode('utf-8'))
+    token = f.encrypt(data)
     return token
 
 def decrypt(key, data):
@@ -46,27 +45,33 @@ def get_password():
         else:
             messagebox.showinfo("Error", "Key generation failed.")
     t_password = Toplevel(master)
+    t_password.attributes('-topmost', 'true')
     password = tkinter.StringVar(t_password)
-    lay.append(t_password)
     Label(t_password, text = "All data is encrypted for privacy. Enter your password:").grid(row = 0)
     pass_enter = Entry(t_password, textvariable = password).grid(row = 1)
     ttk.Button(t_password, text = "OK", command = exit_btn_password).grid(row = 2)
     
-
-
 def popup_reader():
+    global key
     t_read = Toplevel(master)
     txt_box = Text(t_read)
     txt_box.grid(row = 0, column = 0)
-    decoded = []
     if os.path.isfile("moodhistory.csv") == True:
         with open("moodhistory.csv", newline = '') as file:
             reader = csv.reader(file)
-            for row in reader:
-                for each in row:
-                    decoded.append(decrypt(each))
-                txt_box.insert(INSERT, decoded)
-                decoded = []
+            row1 = next(reader, None)
+            header1 = row1[0]
+            header2 = row1[1]
+            header3 = row1[2]
+            txt_box.insert(INSERT, header1 + "                        " + header2 + "   " + header3+"\n")
+            for line in reader:
+                tstamp_enc = line[0][2:-1]
+                rate_enc = line[1][2:-1]
+                note_enc = line[2][2:-1]
+                tstamp = decrypt(key, tstamp_enc.encode('utf-8'))
+                rate = decrypt(key, rate_enc.encode('utf-8'))
+                note = decrypt(key, note_enc.encode('utf-8'))
+                txt_box.insert(INSERT, tstamp + "     " + rate + "     " + note + "\n")
     else:
         txt_box.insert(INSERT, "Data not available yet.")
 
@@ -81,8 +86,8 @@ def clear_data():
         tclear = Toplevel(master)
         tclear.title("Confirmation")
         Label(tclear, text = "Are you sure you want to delete all data?").grid(row = 0, column = 1)
-        ttk.button(tclear, text = "Confirm").grid(row = 1, column = 0)
-        ttk.button(tclear, text = "Cancel", command =exit_btn).grid(row = 1, column = 2)
+        ttk.Button(tclear, text = "Confirm", command=confirm_btn).grid(row = 1, column = 0)
+        ttk.Button(tclear, text = "Cancel", command =exit_btn).grid(row = 1, column = 2)
     else:
         messagebox.showinfo("Error", "Data file is missing or already deleted.")
 
@@ -90,24 +95,46 @@ def save_data():
     global key
     rating = sb.get()
     notes = notes_ent.get()
-    tstamp = datetime.datetime.now()
+    tstamp = str(datetime.datetime.now())
     enc_rating = encrypt(key, rating.encode('utf-8'))
     enc_notes = encrypt(key, notes.encode('utf-8'))
     enc_timestamp = encrypt(key, tstamp.encode('utf-8'))
-    entry = [enc_rating, enc_notes, enc_timestamp]
+    entry = [enc_timestamp, enc_rating, enc_notes]
     if os.path.isfile("moodhistory.csv") == False:
-        with open("moodhistory.csv", newline = '') as file:
+        with open("moodhistory.csv",'w', newline = '') as file:
             writer = csv.writer(file)
-            file.writeline("Date", "Rating", "Notes")
-    else:
-        with open("moodhistory.csv", newline = '') as file:
+            writer.writerow(["Date", "Rating", "Notes"])
+    with open("moodhistory.csv",'a', newline = '') as file:
             writer = csv.writer(file)
-            file.writeline(entry)
+            writer.writerow(entry)
             messagebox.showinfo("Success", "Entry Saved")
+
+def plot_data():
+    tstamps = []
+    rates = []
+    notes = []
+    if os.path.isfile("moodhistory.csv") == True:
+        with open("moodhistory.csv", newline = '') as file:
+            reader = csv.reader(file)
+            row1 = next(reader, None)
+            for line in reader:
+                tstamp_enc = line[0][2:-1]
+                rate_enc = line[1][2:-1]
+                note_enc = line[2][2:-1]
+                tstamp = decrypt(key, tstamp_enc.encode('utf-8'))
+                tstamps.append(tstamp)
+                rate = decrypt(key, rate_enc.encode('utf-8'))
+                rates.append(rate)
+                note = decrypt(key, note_enc.encode('utf-8'))
+                notes.append(note)
+    else:
+        txt_box.insert(INSERT, "Data not available yet.")
+    fig = px.line(x = tstamps, y = rates, text = notes, title ="Mood Change over Time")
+    fig.update_layout(xaxis_title="Date-Time", yaxis_title="Mood Rating")
+    fig.show()
 
 password = ""
 key = ""
-lay=[]
 master = tkinter.Tk(className='Mooder')
 Label(master, text ='Mood Rating').grid(row = 0, column = 0)
 Label(master, text ='Any notes for this entry?').grid(row=1, column = 0)
@@ -115,31 +142,12 @@ sb = Spinbox(master, from_ = 1, to = 5)
 sb.grid(row = 0, column = 1)
 notes_ent = Entry(master)
 notes_ent.grid(row = 1, column = 1)
-save_button = Button(master, text = "Save")
+save_button = Button(master, text = "Save", command = save_data)
 save_button.grid(row = 2, column = 1)
 Label(master, text ='Data Managment:').grid(row=0, column = 2)
 ttk.Button(master, text = "View Data", command = popup_reader).grid(row = 0, column = 3)
-ttk.Button(master, text = "Chart Data").grid(row = 1, column = 3)
+ttk.Button(master, text = "Chart Data", command = plot_data).grid(row = 1, column = 3)
 ttk.Button(master, text = "Clear Data", command = clear_data).grid(row = 2, column = 3)
 get_password()
-while key != "":
-    Label(master, text ='Data file unlocked!').grid(row = 3, column = 0)
 master.mainloop()
 
-
-#working = True
-#password = input("whats the password?\n")
-#while working == True: 
-#    choice = input("Do you want to 1. Encrypt or 2. Decrypt or 3. Exit\n")
-#    if choice == "1":
-#        message = input("Whats the message?\n")
-#        key = password_to_key(password)
-#        enc_message = encrypt(key, message)
-#        print (enc_message)
-#    elif choice == "2":
-#        dec_message = decrypt(key, enc_message)
-#        print(dec_message)
-#    elif choice == "3":
-#        working = False
-#    else:
-#        print("Not a valid choice.\n")
